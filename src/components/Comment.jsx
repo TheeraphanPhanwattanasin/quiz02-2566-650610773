c0047b47688b48e6735827c5df4f8e579c0b407a
b <b>
2023-08-10 14:57:03 +0700

    all completed 1

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,5 @@
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +7,46 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+  let havelike = false;
+  if (likeNum > 0) havelike = true;
+  const allreplies = replies.map((rep) => (
+    <Reply
+      userImagePath={rep.userImagePath}
+      username={rep.username}
+      replyText={rep.replyText}
+      likeNum={rep.likeNum}
+      key={rep.username}
+    />
+  ));
+
   return (
     <div>
-      your code here...
-      {/* map-loop render Reply component here */}
+      <div className="d-flex gap-2 my-2 ">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#3A3B3C" }}
+        >
+          <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {username}
+          </span>
+          <br />
+          <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+          {havelike && (
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span>
+            </div>
+          )}
+        </div>
+      </div>
+      {allreplies}
     </div>
   );
 };
